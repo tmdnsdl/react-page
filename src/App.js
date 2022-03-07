34e78418c8fd2dbcb82937f5f54c8f22bb01ee7e
tmdnsdl <tmdnsdl@hotmail.com>
2022-03-07 16:14:39 +0900

    2022-03-07 update 1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
-import logo from "./logo.svg";
+import refresh from "./refresh.png";
+import loadingImg from "./loading.gif";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+    const [loading, setLoading] = useState(true);
+    const [cat, setCat] = useState("");
+
+    useEffect(() => {
+        fetch("https://api.thecatapi.com/v1/images/search").then((response) =>
+            response.json().then((json) => {
+                setCat(json[0].url);
+                setLoading(false);
+            })
+        );
+    }, []);
+
+    const Refresh = () => {
+        setLoading(true);
+        fetch("https://api.thecatapi.com/v1/images/search").then((response) =>
+            response.json().then((json) => {
+                setCat(json[0].url);
+                setLoading(false);
+            })
+        );
+    };
+
     return (
         <div className="App">
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>너구리 React APP</p>
-                <a
-                    className="App-link"
-                    href="https://tmdnsdl.github.io/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    너구리 블로그
-                </a>
+                <p>고양이 탐색기</p>
+                {loading ? (
+                    <img src={loadingImg} class="loading" />
+                ) : (
+                    <div class="cat-box">
+                        <img src={cat} class="cat-image" />
+                    </div>
+                )}
+                <div style={{ marginTop: "20px" }}>
+                    <input onClick={Refresh} type="image" src={refresh} />
+                </div>
             </header>
         </div>
     );
